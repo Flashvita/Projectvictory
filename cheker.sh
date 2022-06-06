dd1cf142de7169d5022a3c147d00721aa6ec28d1
pavel.milanov <pawel.milanov@yandex.ru>
2022-06-06 21:00:51 +0300

    upload CI file

--- cheker.sh
+++ cheker.sh
@@ -4,7 +4,7 @@ CONTAINER_NAME=$1
 OUTPUT=$(docker ps | awk '{print $15}')
 
 if [ $OUTPUT == $CONTAINER_NAME ]; then
-	$(docker stop ${CONTAINER_NAME})
+	docker stop ${CONTAINER_NAME}
 else
 	echo "контейнер не запущен"
 fi
