1df35fcdf66801c16794128158e6e4709ba65507
pavel.milanov <pawel.milanov@yandex.ru>
2022-06-06 20:55:32 +0300

    add CI file

---
+++ cheker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CONTAINER_NAME=$1
+OUTPUT=$(docker ps | awk '{print $15}')
+
+if [ $OUTPUT == $CONTAINER_NAME ]; then
+	$(docker stop ${CONTAINER_NAME})
+else
+	echo "контейнер не запущен"
+fi
