749054c86296f46e700eec235e4a1ad1767a03ff
Pavel Milanov <milanov71@icloud.com>
2022-06-08 15:42:49 +0300

    fix bag from smtp

--- Backend/victory/victory/settings.py
+++ Backend/victory/victory/settings.py
@@ -143,11 +143,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # SMTP Email settings
-EMAIL_USE_TLS = True
+# EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'DjangoTestemail2022@gmail.com'
 EMAIL_HOST_PASSWORD = '1234dx1234dx'
-EMAIL_PORT = 587
+EMAIL_PORT = 25  # 587
 
 # Cors settings
 CORS_ALLOW_ALL_ORIGINS = True
