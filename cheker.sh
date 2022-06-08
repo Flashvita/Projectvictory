e7d00a6d6a2a73953e7a1776fb0c2a0609288b7b
Pavel Milanov <milanov71@icloud.com>
2022-06-08 15:48:16 +0300

    fix

--- cheker.sh
+++ cheker.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
 CONTAINER_NAME=$1
-OUTPUT=$(docker ps | awk '{print $15}')
 
-if [ $CONTAINER_NAME ]; then
-	docker stop ${CONTAINER_NAME}
-else
-	echo "контейнер не запущен"
-fi
+docker stop ${CONTAINER_NAME}
