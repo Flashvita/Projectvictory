7b2f3723624edd42e3975b5961f8603a2185e282
Pavel Milanov <milanov71@icloud.com>
2022-06-29 11:17:47 +0300

    fix

--- Backend/victory/init.sh
+++ Backend/victory/init.sh
@@ -6,7 +6,7 @@ set -e
 
 python3 manage.py makemigrations mainapp
 python3 manage.py migrate
-python3 manage.py collectstatic -y
+python3 manage.py collectstatic
 
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@main.ru', 'admin')" | python manage.py shell
 
