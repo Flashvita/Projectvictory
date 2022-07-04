f7cd274269b3075254bfe93f328d1e5b8bacc25f
Pavel Milanov <milanov71@icloud.com>
2022-07-04 05:49:23 +0000

    Update Backend/victory/init.sh

--- Backend/victory/init.sh
+++ Backend/victory/init.sh
@@ -8,6 +8,12 @@ python3 manage.py makemigrations mainapp
 python3 manage.py migrate
 echo yes | python3 manage.py collectstatic
 
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@main.ru', 'admin')" | python manage.py shell
+default_admin=$("from django.contrib.auth import get_user_model; User = get_user_model(); admin=User.objects.get(username='admin'); print(admin)" | python manage.py shell)
+
+if [ $default_admin=='admin' ]; then
+    echo 'admin is exsist'
+else
+    echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@main.ru', 'admin')" | python manage.py shell
+fi
 
 gunicorn -b ${SERVER_IP}:8000 victory.wsgi:application
