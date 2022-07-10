13566b0b98615ae4165f486306a5c5e7cbf97705
korytovaf <kaf1@mail.ru>
2022-07-10 11:51:05 +0300

    hid the button if not the admin

--- Frontend/src/components/Articles/ArticleItem.vue
+++ Frontend/src/components/Articles/ArticleItem.vue
@@ -1,8 +1,8 @@
 <template>
   <li class="article">
-    <div class="article-owner">
-      <div class="article-avatar">
-        <div v-if="!article.avatar" class="article-avatar-placeholder"></div>
+    <div class="article__owner">
+      <div class="avatar">
+        <div v-if="!article.avatar" class="avatar__placeholder" />
         <img
           v-else
           :src="'http://45.8.248.219/media/' + article.avatar"
@@ -11,41 +11,35 @@
       </div>
       <div>{{ article.owner }}</div>
     </div>
-    <h2 class="article-title">{{ article.title }}</h2>
-    <div class="article-body" v-html="article.content"></div>
-    <div class="article-button-block">
-      <MyButton
-        class="article-btn"
+    <h2 class="article__title">{{ article.title }}</h2>
+    <div class="article__body" v-html="article.content"></div>
+    <div class="article__buttons">
+      <my-button
+        class="btn__next"
         @click="$router.push(`/articles/${article.id}`)"
       >
         Читать далее
-      </MyButton>
-      <div
-        class="icon-remove"
+      </my-button>
+      <my-button
+        v-if="profile.is_admin"
+        class="btn__basket"
         @click="this.onRemove(article.id, this.$route.query)"
       >
-        <IconBase
-          iconName="remove-icon"
-          width="20"
-          heigth="20"
-          view-box="0 0 448 512"
-        >
-          <remove-basket />
-        </IconBase>
-      </div>
+        <icons-component name="basket" class="btn__basket_icon" />
+      </my-button>
     </div>
   </li>
 </template>
 
 <script>
-import RemoveBasket from "@/assets/icons/remove-basket";
-import IconBase from "@/components/UI/IconBase";
-import { mapActions } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 import axios from "axios";
+import MyButton from "@/components/UI/MyButton";
+import IconsComponent from "@/assets/icons/icons-component";
 
 export default {
   name: "ArticleItem",
-  components: { IconBase, RemoveBasket },
+  components: { MyButton, IconsComponent },
 
   props: {
     article: Object,
@@ -67,6 +61,11 @@ export default {
       await this.getArticleOll(query);
     },
   },
+  computed: {
+    ...mapGetters({
+      profile: "auth/profile",
+    }),
+  },
 };
 </script>
 
@@ -78,12 +77,40 @@ export default {
   background-color: var(--color-white);
   border-radius: 3px;
 
-  .article-button-block {
+  &__owner {
+    display: flex;
+    border-radius: 3px;
+    margin-bottom: 10px;
+    align-items: flex-end;
+
+    & .avatar {
+      width: 30px;
+      margin-right: 10px;
+      border-radius: 3px;
+
+      &__placeholder {
+        width: 100%;
+        height: 100%;
+        background-color: var(--color-bacground-grey);
+        border-radius: 3px;
+      }
+    }
+  }
+
+  &__title {
+    margin-top: 20px;
+  }
+
+  &__body {
+    margin: 20px 0;
+  }
+
+  &__buttons {
     display: flex;
     justify-content: space-between;
     align-items: flex-end;
 
-    .article-btn {
+    & .btn__next {
       border-radius: 3px;
       background-color: transparent;
       border: 1px solid var(--color-accent);
@@ -95,39 +122,25 @@ export default {
         color: var(--color-white);
       }
     }
-  }
 
-  .icon-remove {
-    color: var(--color-bacground-grey);
-    cursor: pointer;
-    transition: all 0.3s;
-    &:hover {
-      color: var(--color-accent);
+    & .btn__basket {
+      width: 30px;
+      height: 30px;
+      padding: 0;
+      border-radius: 50%;
+      background-color: transparent;
+
+      &:hover .btn__basket_icon {
+        fill: var(--color-accent);
+      }
+
+      &_icon {
+        width: 20px;
+        height: 20px;
+        fill: var(--color-bacground-grey);
+        transition: all 0.2s;
+      }
     }
   }
-}
-.article-owner {
-  display: flex;
-  border-radius: 3px;
-  margin-bottom: 10px;
-  align-items: flex-end;
-}
-
-.article-avatar {
-  width: 30px;
-  height: 30px;
-  margin-right: 10px;
-  border-radius: 3px;
-}
-
-.article-avatar-placeholder {
-  width: 100%;
-  height: 100%;
-  background-color: var(--color-bacground-grey);
-  border-radius: 3px;
-}
-
-.article-body {
-  margin: 20px 0;
 }
 </style>
